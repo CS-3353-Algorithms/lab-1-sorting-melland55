c717fcfbb1ef15097b6eb352bd76a986f8afc4dc
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 23:08:26 -0500

    Created Base Class Algorithm

---
+++ Code/Algorithm.h
@@ -0,0 +1,27 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include <string>
+using namespace std;
+
+class Algorithm
+{
+    public:
+    	//Creates an enum with every type of sorting algorithm
+    	enum SortingAlgo{
+        	BUBBLE = 0,
+        	MERGE,
+        	INSERTION,
+        	END
+		};
+    	
+    	//Defines each function that will be overloaded in the Sort class
+        virtual void load(string) = 0;
+        virtual void execute() = 0;
+        virtual void display() = 0;
+        virtual void stats() = 0;
+        virtual void select(int) = 0;
+        virtual void save(string) = 0;
+        virtual void configure() = 0;
+};
+#endif
